564ced3e83e1aea99a0b00f4d2822671357d5ceb
Yuhang Chen <i@yuhang.ch>
2020-08-09 16:42:07 +0800

    yaml example

---
+++ api/tiles_test.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"testing"
+)
+
+// Test .
+func TestTemplate(t *testing.T) {
+
+	// tile := mercantile.NewTile(1, 2, 3)
+	// // bbox := tile.XYBounds()
+	// // segSize := (bbox.Right - bbox.Left) / 4
+	// // fmt.Println(bbox.List())
+	// ctx := NewTilesCtx("countries", "geom", "fid", tile)
+	// query := sql(ctx)
+	// fmt.Println(query)
+}
